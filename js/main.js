0ea7d79d05a1a21761d89c0db36d1b7d60a68d4f
Richard Lee Parkin <mrparkin@hotmail.co.uk>
2019-07-15 09:09:40 +0100

    switch computers
    +ES6 intellisence
    +rules on stack
    ____________________
    still need to:
    
    >stort out the deck when array runs out and make sure it desnt give duplicate cards
    
    >add rules to the suits stack
    
    >pick up all childnodes when shifting within the stack

--- js/main.js
+++ js/main.js
@@ -1,45 +1,110 @@
 function redOrBlack (string){
-    if(string.toLocaleLowerCase().includes("diamonds")||string.toLocaleLowerCase().includes("hearts")){
-        return "red"
+    if(string.toLowerCase().includes("diamonds")||string.toLowerCase().includes("hearts")){
+        return "red";
     }
     else{
-        return "black"
+        return "black";
     }    
 }
 function convertToNumber(cardNo){
-    if(cardNo=="A"){
+    if(cardNo.includes("A")){
         return 1;
     }
-    if(cardNo=="J"){
+    if(cardNo.includes("J")){
         return 11;
     }
-    if(cardNo=="Q"){
+    if(cardNo.includes("Q")){
         return 12;
     }
-    if(cardNo=="K"){
+    if(cardNo.includes("K")){
         return 13;
     }
+    else{return parseInt(cardNo);}
 }
 
-function higherOrLower(dragged, target){
 
-    if(dragged>target){
-        return "false";
+function higherOrLower(selected, target){
+    selected = convertToNumber(selected.toString());
+    target = convertToNumber(target.toString());
+
+    console.log(selected+" , "+target);
+
+    if(selected!=target-1){
+        return false;
     }
 }
 
+
+function checkForFlip(_selectedCardParentElement){
+    //does sleected card have a card in previous stack to flip
+    if(_selectedCardParentElement.childNodes.length >0){
+        const checkIfLastCardNeedsTurning = document.getElementById(_selectedCardParentElement.id).lastElementChild.classList[0];
+        const lastchildId = document.getElementById(_selectedCardParentElement.id).lastElementChild.id;
+
+        if(checkIfLastCardNeedsTurning.includes("faceUp")==false){
+            document.getElementById(lastchildId).classList.remove("facedown");
+            document.getElementById(lastchildId).classList.add("faceUp");
+            document.getElementById(lastchildId).draggable ="true";
+        }
+    }
+
+}
+
+function dropOntoLastChild(eventTarget, _selectedCardElement, _data){
+    const eventTargetParent = eventTarget.parentElement;
+    const _selectedCardParentElement = _selectedCardElement.parentElement;
+    //only drop on the last card of stack
+    if(eventTargetParent.lastElementChild || eventTargetParent.id.includes("stack")){
+        
+        //MOVE CARD
+        //console.log(eventTarget.className);
+
+        if(eventTarget.className.includes("suits") || eventTargetParent.className.includes("suits")){
+            document.getElementById(_data).style.top = "";
+            if(eventTarget.className.includes("faceUp")){
+                eventTargetParent.appendChild(document.getElementById(_data));
+            }else{
+                eventTarget.appendChild(document.getElementById(_data));
+            }
+        }
+        else
+        {
+            //console.log(eventTarget.style.top);
+            if(_selectedCardElement.className.includes("newCard")){
+                _selectedCardElement.classList.remove("newCard");
+            }
+            //stack overlap cards
+            eventTargetParent.appendChild(document.getElementById(_data));
+            var newTopSpace = eventTarget.style.top;
+            newTopSpace = parseInt(newTopSpace.replace("%",""))+20;
+            //console.log(newTopSpace);
+            _selectedCardElement.style.top = newTopSpace+"%";
+        }
+        //is selected from deck?
+        if(_selectedCardParentElement.id=="deckShown")
+        {
+           return;
+        }
+
+        checkForFlip(_selectedCardParentElement);
+
+       
+
+    }
+}
 
 
 document.addEventListener("dragstart",function(ev){
-    ev.dataTransfer.setData("text", ev.target.id);
-    ev.target.style.opacity ="0.7";
+  let data =  ev.dataTransfer.setData("text", ev.target.id);
+  console.log(document.getElementById(data).nextElementSibling);
+  //  ev.target.style.opacity ="0.7";
 });
 document.addEventListener("dragover", function(ev){
     ev.preventDefault();
 });
 document.addEventListener("dragenter", function(event){
     if(event.target.className == "faceUp" || event.target.id.includes("stack")){
-        event.target.style.border = "3px solid blue";
+        event.target.style.border = "9px solid yellow";
     }
 });
 document.addEventListener("dragleave", function(event) {
@@ -52,127 +117,99 @@ document.addEventListener("dragleave", function(event) {
 document.addEventListener("drop", function(ev){
     ev.preventDefault();
     ev.target.style.border = "";
-    var data = ev.dataTransfer.getData("text");
-    var cardType = document.getElementById(data).textContent;
-    var comesFromParent = document.getElementById(data).parentElement.id;
-    var redCards = cardType.toLocaleLowerCase().includes("diamonds")||cardType.toLocaleLowerCase().includes("hearts");
-    console.log(redOrBlack(ev.target.textContent));
-    //is card read or black ?
-    if(redOrBlack(ev.target.textContent) != redOrBlack(cardType)){
-        //need to split the text content, convert A,J,Q,K and do higher or lower function 
-        var firstWord = [];
-        for(var i=0; i<ev.target.textContent; i++){
-            var words = ev.target.textContent[i].split(" ");
-            firstWord.push(words[0]);
-        }
+    const data = ev.dataTransfer.getData("text");
 
-        for(var x in firstWord){
-        console.log(firstWord[x]);
-        }
+    let selectedCard = document.getElementById(data);
+    let selectedCardType = selectedCard.textContent;
+    let selectedCardParent = selectedCard.parentElement;
 
-
-
-        //tell GUI where its allowed to stack    && append to the parent + overlap stack
-        if(ev.target.id.includes("faceUpCard") ){
-            document.getElementById(data).style.top = ev.target.parentElement.childElementCount+"0%";
-            //insert element
-            ev.target.parentElement.appendChild(document.getElementById(data));
-            ev.target.style.opacity ="1";
-
-            var checkIfLastCardNeedsTurning = document.getElementById(comesFromParent).lastElementChild.classList[0];
-            var lastchildId = document.getElementById(comesFromParent).lastElementChild.id;
-
-            if(checkIfLastCardNeedsTurning.includes("faceUp")==false){
-                document.getElementById(lastchildId).classList.remove("facedown");
-                document.getElementById(lastchildId).classList.add("faceUp");
-                document.getElementById(lastchildId).draggable ="true";
-            }
-
-        }
-        else if(ev.target.id.includes("stack"))
-        {
-            document.getElementById(data).style.top=ev.target.parentElement.childElementCount+"0%";
-            //insert element
-            ev.target.appendChild(document.getElementById(data));
-            document.getElementById(data).style.opacity ="1";
-            document.getElementById(data).style.top = "";
-
-        }
+  
+    if(ev.target.tagName=="html"){return;}
+    else if(ev.target.tagName !== "DIV" && ev.target.parentElement.classList[0].includes("cardPlaceholder") == false ){
+        return;
     }
+    else{
+        //if card dropped on suit stack
+        if(ev.target.parentElement.className.includes("suits")|| ev.target.className.includes("suits")){
+            dropOntoLastChild(ev.target, selectedCard, data);
+        }
+        //if card is dropped on empty stack
+        else if(ev.target.className.includes("cardPlaceholder") && selectedCardType.toLowerCase().includes("k")){
+            document.getElementById(data).style.top ="";
+            ev.target.appendChild(document.getElementById(data));
+            checkForFlip(selectedCardParent);
+        }
+        //if card dropped on stack
+        else if(redOrBlack(ev.target.textContent) != redOrBlack(selectedCardType)){
+            if(higherOrLower(selectedCard.textContent,ev.target.textContent)==false)
+            {
+                console.log("it was false");
+                return;
+            }
+            dropOntoLastChild(ev.target, selectedCard, data);
+            
+        }
+
+    }
+
 });
 
 
 
-var deck= [];
+
+
+let deck= [];
 //DECK ---- JQUERY
 $(document).ready(function(){
 
-
-    var cardRange =["A","2","3","4","5","6","7","8","9","10","J","Q","K" ];
-   
-    var i;
-    for(i=0; i< cardRange.length; i++){
+    let cardRange =["A","2","3","4","5","6","7","8","9","10","J","Q","K" ];
+       
+    for(let i=0; i< cardRange.length; i++){
         deck.push(cardRange[i]+ " Spades");
         deck.push(cardRange[i]+ " Hearts");
         deck.push(cardRange[i]+ " Clubs");
-        deck.push(cardRange[i]+ " Diamonds");
-       
+        deck.push(cardRange[i]+ " Diamonds");      
     }
   
     deck.sort(function(a,b) {return 0.5 - Math.random();});
 
-    console.log(deck.length);
 
-
-
-
-    //deal [0,1,2,3,4,5,6] = 21 | 52-21 =  31
-
-    var dealCounter; var stackPosition = 1; var rowcount =0; var stackCards_topPercentage = 0;
-
+    let dealCounter; let stackPosition = 1; let rowcount =0; let stackCards_topPercentage = 0;
     for(dealCounter=0; dealCounter<=27; dealCounter++)
     {
         //create card
-        var currentCard = deck[dealCounter];
-        var faceDownCard = document.createElement('div');  
+        const currentCard = deck[dealCounter];
+        const faceDownCard = document.createElement('div');  
         faceDownCard.innerHTML = currentCard;     
         faceDownCard.id = "faceDownCard"+dealCounter;
-        faceDownCard.style.top = stackCards_topPercentage+"%"
-
-    
+        faceDownCard.style.top = stackCards_topPercentage+"%";
         faceDownCard.className = "facedown";
-      
-        var faceUpCard = document.createElement("div");
+        changeColour(faceDownCard);
+
+        const faceUpCard = document.createElement("div");
         faceUpCard.id = "faceUpCard"+dealCounter;
         faceUpCard.innerHTML=currentCard;
         faceUpCard.draggable="True";
         faceUpCard.classList.remove("facedown");
         faceUpCard.classList.add("faceUp");
-
+        changeColour(faceUpCard);
 
         if(dealCounter==0)
         {
-         
             document.getElementById("stack"+stackPosition).appendChild(faceUpCard);
         }
        
-
-
         //change rows        
         else if(stackPosition>7)
-        { 
- 
-            stackCards_topPercentage += 10;
-            faceUpCard.style.top = stackCards_topPercentage+"%"
-         
-           
-            //ondrop="drop(event)" ondragover="allowDrop(event)"  draggable="true" ondragstart="drag(event)
-            
+        {  
+            stackCards_topPercentage += 15;
+            faceUpCard.style.top = stackCards_topPercentage+"%";
+
             //each row start new pos
             stackPosition=0;
             rowcount ++;
-            for(var temp=0; temp<rowcount; temp++){
-                stackPosition=temp+2
+            for(let temp=0; temp<rowcount; temp++){
+                stackPosition=temp+2;
             }
            
             document.getElementById("stack"+stackPosition).appendChild(faceUpCard);
@@ -181,51 +218,45 @@ $(document).ready(function(){
             document.getElementById("stack"+stackPosition).appendChild(faceDownCard);
         }
    
-       
-      
-
-        
-        stackPosition++;
-       
-
-
-
-
-
-
-
-                
-     
+        stackPosition++; 
     }
-
-
 });
 
 
-
-
-
-
-
-
-
-
-
+function changeColour(cardElement){
+    if(cardElement.innerHTML.toLowerCase().includes("heart")||cardElement.innerHTML.toLowerCase().includes("diamonds")){
+        cardElement.classList.add("redCard");
+    }
+    else{
+        cardElement.classList.add("blackCard");
+    }
+}
 
 //deck click event
-var i=0;
+let deckDrawnCounter=0;
 function reveal(ev){
     
-    var cardFromDeck = document.createElement("div");
-    cardFromDeck.id = "cardDrawn"+i;
+    const cardFromDeck = document.createElement("div");
+    cardFromDeck.id = "cardDrawn"+deckDrawnCounter;
     cardFromDeck.className = "cardPlaceholder newCard faceUp";
     document.getElementById("deckShown").appendChild(cardFromDeck);
     cardFromDeck.draggable = "true";
-    const cardDrawn= document.getElementById("cardDrawn"+i);
+    const cardDrawn = document.getElementById("cardDrawn"+deckDrawnCounter);
     cardDrawn.style.display = "block";
 
-
-    cardDrawn.innerHTML = deck[i+21];
-    i++;
+    let leftOverCards =[];
+    leftOverCards = deck.shift(27);
+    
+    if(deckDrawnCounter===27){
+        cardFromDeck.style.background = "green";
+    }
+    else if(deckDrawnCounter>27){
+        deckDrawnCounter=0;
+    }
+    else{
+    cardDrawn.innerHTML = leftOverCards[deckDrawnCounter];
+    }
+    changeColour(cardDrawn);
+    deckDrawnCounter++;
 }
 
